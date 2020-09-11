7a9d49e962e65e8abf019d51ed9e367ea1ab25d2
Nikhil <nikhilyadav3000@gmail.com>
2020-09-12 01:31:31 +0530

    backendURL updated

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  backendBaseURL: 'http://localhost:5000',
+  backendBaseURL: 'https://tezos-backend.herokuapp.com',
   oracleContract: 'KT1X7RZUdceFctHxXY84zZzMpgrSfe83TfQQ',
   node: 'https://testnet-tezos.giganode.io',
 };
